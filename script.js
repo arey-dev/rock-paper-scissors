fc287559942b1edbb28b6261e18aa36718dac498
R.A Borja <raborja.dev@gmail.com>
2022-09-01 16:40:42 +0800

    create function game()

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
 /**
+ * Performs mulitple rounds of Rock Paper Scissors base on user input
+ * Keeps a score of player and computer in all rounds.
+ * 
+ * @return {string} returns the score and winner of the game
+ */
+function game() {
+  let playerScore = 0, computerScore = 0;
+
+  const numOfRounds = +prompt("For how many rounds do you want to play?", 1);
+
+  for(let i = 0; i < numOfRounds; i++) {
+    let playerChoice = prompt("Enter the choice of hand you want ot play");
+    let roundResult = playRound(playerChoice, getComputerChoice());
+    if(roundResult.includes('Win')) playerScore++;
+    else if(roundResult.includes('Lose')) computerScore++;
+
+    console.log(roundResult);
+  }
+
+  console.log(`Game ends! Final scores are, Player: ${playerScore} Computer: ${computerScore}`);
+
+  if(playerScore > computerScore) console.log("Player Wins");
+  else if(playerScore < computerScore) console.log("Computer Wins");
+  else console.log("No one wins! It's a draw!");
+}
+
+/**
  * Plays a single round of Rock Paper Scissors
  * 
- * @param {string} playerSelection an input from a player, not case sensitive
- * @param {string} computerSelection an input from the opponenet, computer generated
- * @return {string} returns a declaration of wether or not the player wins
+ * @param {string} playerSelection a choice of hand to play, not case sensitive
+ * @param {string} computerSelection computer generated choice of hand to play
+ * @return {string} returns a string that says wether or not the player wins
  */
 function playRound(playerSelection, computerSelection) {
   // capitalize player input
@@ -39,7 +66,7 @@ function playRound(playerSelection, computerSelection) {
 /**
  * Randomly generates a choice for computer to play
  * 
- * @return {string} returns either "Rock", "Paper", or "Scissors"
+ * @return {string} returns a string, either "Rock", "Paper", or "Scissors"
  */
 function getComputerChoice() {
   const num = randomNumber();
